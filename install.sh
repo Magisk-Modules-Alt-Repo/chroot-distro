6b3cae8dba18d46db4bfdca977edcfdfb9cdb1ff
YasserNull <162287858+YasserNull@users.noreply.github.com>
2024-03-05 18:07:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
+#!/system/bin/sh
+
 if [ ! -d "/data/local/tmp/" ]; then
     mkdir /data/local/tmp/
 fi
 curl -o /data/local/tmp/chroot-distro https://raw.githubusercontent.com/YasserNull/chroot-distro/main/chroot-distro
-if [ ! -d "/system/etc/init.d/" ]; then
-    mkdir /system/etc/init.d/
-fi
-curl -o /system/etc/init.d/chroot-distro.rc https://raw.githubusercontent.com/YasserNull/chroot-distro/main/chroot-distro.rc
+
+curl -o /data/local/tmp/chroot-distro.rc https://raw.githubusercontent.com/YasserNull/chroot-distro/main/chroot-distro.rc
+chmod 755 /data/local/tmp/chroot-distro.rc
 while IFS= read -r line; do
+$line/su /data/local/tmp/chroot-distro.rc &
     mount -o remount,rw $line
    cp /data/local/tmp/chroot-distro $line
    mount -o remount,rw $line
