3c89c61c97d0e91e5d61327595ed48b7129c4498
YasserNull <162287858+YasserNull@users.noreply.github.com>
2024-03-05 18:09:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ curl -o /data/local/tmp/chroot-distro https://raw.githubusercontent.com/YasserNu
 curl -o /data/local/tmp/chroot-distro.rc https://raw.githubusercontent.com/YasserNull/chroot-distro/main/chroot-distro.rc
 chmod 755 /data/local/tmp/chroot-distro.rc
 while IFS= read -r line; do
-$line/su /data/local/tmp/chroot-distro.rc &
-    mount -o remount,rw $line
-   cp /data/local/tmp/chroot-distro $line
-   mount -o remount,rw $line
+/$line/su /data/local/tmp/chroot-distro.rc &
+    mount -o remount,rw /$line
+   cp /data/local/tmp/chroot-distro /$line
+   mount -o remount,rw /$line
 done <<< "$PATH | tr ':' '\n'"
