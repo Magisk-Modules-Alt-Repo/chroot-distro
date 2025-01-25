876f1e8e873c75e04ad2c81406d8aaf06f80ca7d
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-01-25 13:18:21 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,16 +1,18 @@
-MODID=chroot-distro
-AUTOMOUNT=true
-PROPFILE=false
-POSTFSDATA=false
-LATESTARTSERVICE=false
+if [ "$APATCH" ]; then
+    ui_print "- Installing in APatch: $APATCH_VER │ $APATCH_VER_CODE"
+    mode=APatch
+elif [ "$KSU" ]; then
+    ui_print "- Installing in KSU: $KSU_KERNEL_VER_CODE │ $KSU_VER_CODE"
+    mode=KernelSU
+elif [ "$MAGISK_VER_CODE" ]; then
+    ui_print "- Installing in Magisk: $MAGISK_VER │ $MAGISK_VER_CODE"
+    mode=Magisk
+fi
+[ ! -d /data/adb/modules/busybox-ndk ] && ui_print "- Warning: You are installing this module in $mode without busybox-ndk installed"
+dos2unix "$MODPATH/system/bin/chroot-distro"
+set_perm_recursive "$MODPATH/system" 0 0 0755 0755
+ui_print "chroot-distro installation completed"
 
-print_modname() {
-  ui_print "*******************************"
-  ui_print "               chroot-distro                "
-  ui_print "*******************************"
-}
-dos2unix /system/bin/chroot-distro
-REPLACE=""
-set_permissions() {
-  set_perm_recursive  $MODPATH  0  0  0755  0644
-}
+
+
+
